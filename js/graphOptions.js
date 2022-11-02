0f8696eb36178de394d293a7fd80122e60cc4215
Ivan <vankata294n4@gmail.com>
2022-11-02 12:59:33 +0200

    Added functionality to graph option ender eye

--- js/graphOptions.js
+++ js/graphOptions.js
@@ -13,7 +13,27 @@ const graphImage = document.getElementById('graphImage');
             100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 20, 50, 100, 111, 140, 170, 210]
             updateGraph(lineGraph, data)
         })
- 
+
+        document.getElementById('enderEye').addEventListener('click', () => {
+            let stringData = '265474412336070826161184345391715209996887366966452867359152978710944414194889554643873925129169503175932681321807871472522584230532423631885418140365727719083100867659956298793738275764563249';
+            const numData = [];
+            let cNum = '';
+            for (let i = 0; i < stringData.length; i++) {
+                cNum += stringData[i];
+                if (i%2 == 0) {
+                    numData.push(Number(cNum))
+                    cNum = '';
+                }
+            }
+
+            updateGraph(lineGraph, numData)
+        })
+
+        function graphChoiceGenerator(elId, data, graphUpdate) {
+            document.getElementById(elId).addEventListener('click', 
+            () => updateGraph(data, graphUpdate))
+        }
+
         graphImage.addEventListener('click', () => {
             graphImage.style.display = 'none';
             graphOptionsDiv.style.display = 'block';
