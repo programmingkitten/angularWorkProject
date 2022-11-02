bf6864e26a571eca8b2831ab955397081047855e
Ivan <vankata294n4@gmail.com>
2022-11-02 13:14:01 +0200

    Improved giving data to graphUpdater by making an obj to take care of data

--- js/graphOptions.js
+++ js/graphOptions.js
@@ -1,20 +1,19 @@
 const graphImage = document.getElementById('graphImage');
         const graphOptionsDiv = document.getElementById('graphOptions');
-       
+        const dataGraphOptions = {
+            'angelOfDeath':  [30, 100, 150,1, 30, 60, 90, 150, 210, 180, 150, 100, 50, 
+                10, 30, 100, 150, 30, 100, 150,30, 100, 150, 30, 
+                100, 150,30, 100, 150,120, 150,200, 160, 130, 100, 65, 20],
+            'excessSky': [1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 20, 50, 100, 111, 140, 170, 210],
+            'enderEye': groupNumbersFromString(
+                '265474412336070826161184345391715209996887366966452867359152978710944414194889554643873925129169503175932681321807871472522584230532423631885418140365727719083100867659956298793738275764563249'),
+        }
 
-        graphUpdater('angelOfDeath', 
-        [30, 100, 150,1, 30, 60, 90, 150, 210, 180, 150, 100, 50, 
-        10, 30, 100, 150, 30, 100, 150,30, 100, 150, 30, 
-        100, 150,30, 100, 150,120, 150,200, 160, 130, 100, 65, 20], lineGraph)
+        graphUpdater('angelOfDeath', dataGraphOptions.angelOfDeath, lineGraph)
 
-        graphUpdater('excessSky', 
-        [1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 1, 100, 100, 100, 100, 100, 1, 20, 50, 100, 111, 140, 170, 210],
-        lineGraph)
+        graphUpdater('excessSky', dataGraphOptions.excessSky,lineGraph)
 
-        graphUpdater('enderEye', groupNumbersFromString(
-            '265474412336070826161184345391715209996887366966452867359152978710944414194889554643873925129169503175932681321807871472522584230532423631885418140365727719083100867659956298793738275764563249',
-            lineGraph
-            ))
+        graphUpdater('enderEye', dataGraphOptions.enderEye ,lineGraph)
 
 
         function groupNumbersFromString(stringData) {
