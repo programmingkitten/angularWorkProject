0bbad83f73a3a31c74fc7f3a748a68802fe66409
Ivan <vankata294n4@gmail.com>
2022-11-03 14:54:47 +0200

    Added hover eff and selectment of elements

--- js/aboutBoxController.js
+++ js/aboutBoxController.js
@@ -1,6 +1,19 @@
-
+import {textData} from "./textData.js"
 const aboutBoxDiv = document.getElementsByClassName('aboutBox')[0];
 
-for (let children of aboutBoxDiv.children) {
-    console.log(children.textContent)
+
+for (let i=0; i< aboutBoxDiv.children.length-1; i++) {
+    aboutBoxDiv.children[i].addEventListener('click', showOption)
+}
+
+function showOption(ev) {
+    let el = ev.target;
+    clearHover();
+    el.style.color = 'indianred';
+}
+
+function clearHover() {
+    for (let i=0; i<aboutBoxDiv.children.length-1; i++) {
+        aboutBoxDiv.children[i].style.color = 'rgb(57, 56, 56)'
+    }
 }
