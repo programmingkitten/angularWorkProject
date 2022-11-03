b2fd0e8373b0f421991da68c9ad33f9c48b8dc5d
Ivan <vankata294n4@gmail.com>
2022-11-03 15:02:34 +0200

    Connected text obj to template

--- js/aboutBoxController.js
+++ js/aboutBoxController.js
@@ -1,6 +1,7 @@
 import {textData} from "./textData.js"
 const aboutBoxDiv = document.getElementsByClassName('aboutBox')[0];
-
+const infoText = aboutBoxDiv.children[aboutBoxDiv.children.length-1];
+console.log(infoText)
 
 for (let i=0; i< aboutBoxDiv.children.length-1; i++) {
     aboutBoxDiv.children[i].addEventListener('click', showOption)
@@ -9,6 +10,9 @@ for (let i=0; i< aboutBoxDiv.children.length-1; i++) {
 function showOption(ev) {
     let el = ev.target;
     clearHover();
+    console.log(el.textContent)
+    console.log(textData[el.textContent])
+    infoText.textContent = textData[el.textContent];
     el.style.color = 'indianred';
 }
 
