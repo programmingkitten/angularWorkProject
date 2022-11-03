7ee93bc0c273da86195e251c19278c8e453f54a0
Ivan <vankata294n4@gmail.com>
2022-11-03 14:53:14 +0200

    Wrote an obj that contains all the data for about page

---
+++ js/textData.js
@@ -0,0 +1,58 @@
+
+
+
+
+export const textData = {
+    'My life': `
+    
+    I was born on 4th octomber in 2004. A couple
+    years later my parents moved to General Toshevo.
+    I grew up in a very nice class with
+    every kid being nice and good in school.
+    This environment helped got a fabulous childhood.
+    I never really had any interests or hobbies. I would
+    just play games all day and go out with
+    my friends. A very simple life. However
+    When i started 8th grade i found an 
+    udemy course that i wanted to try. 
+    This was the first step to start coding.
+    Later on i found about softuni and got
+    excellent programming knowledge of three
+    languages. In school we started studying
+    web design so I knew how to use a technolodgy
+    for both front and back end.
+
+    `,
+
+    'Hobbies': `
+    
+    I enjoy playing games with cars. I love solving the rubik's cube.
+    Motorcycles are also a huge hobby of mine.
+    More of riding them than fixing them tho.
+    My favourite hobby is programming but it's
+    not a hobby anymore since I have decided 
+    to do programming as my job in the future.
+    `,
+
+    'Dreams': `
+    I dream to dravel the world.. to own an 8 series BMW
+    to own the newest BMW sport motorcycle called S1000R.
+    I dream to be enough financially stable to provide for
+    my family and make sure they don't have to work.
+    I dream to have children and play kids with them, drive mini bikes
+    with them and go kart. I dream to have a couple cats
+    even tho i have one right now. I dream to be happy.. 
+    I dream to forget her.
+    
+    `,
+
+    'Portfolio of projects': `
+    I have three big projects that can be found on my github
+    which is github.com/programmingkitten`,
+
+    'Interesting facts about me': `
+    I used to play hockey on grass and even have 20 medals! Also i have went to 
+    swimming lessons for a whole year. My father is a diver.
+    
+    `,
+}
