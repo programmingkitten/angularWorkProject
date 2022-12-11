9b941511b723b64fb7d6cf51f1cbac9d4e21b158
Ivan <vankata294n4@gmail.com>
2022-12-11 18:17:51 +0200

    setting up routes

--- project/frontend/graphApp/src/app/app-routing.module.ts
+++ project/frontend/graphApp/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'users/list',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'users/list', 
+    component: UserListComponent,
+  },
+
+  {
+    path: 'users/:id', component: UserDetailsComponent,
+  },
+
+  {
+    path: 'users/add', component: AddUserComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/frontend/graphApp/src/app/user/user.module.ts
+++ project/frontend/graphApp/src/app/user/user.module.ts
@@ -14,6 +14,11 @@ import { UserListComponent } from './user-list/user-list.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    AddUserComponent,
+    UserDetailsComponent,
+    UserListComponent,
   ]
 })
 export class UserModule { }
