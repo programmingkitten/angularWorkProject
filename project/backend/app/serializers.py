a58b585d10f748b357a76d194429d70ee4463fef
Ivan <vankata294n4@gmail.com>
2022-12-11 16:44:28 +0200

    fixed typo

--- project/backend/app/serializers.py
+++ project/backend/app/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from app.models import UserTest
 
 
-class TutorialSerializer(serializers.ModelSerializer):
+class UserTestSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserTest
         fields = ('id',
