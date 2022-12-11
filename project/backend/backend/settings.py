e00d2014068508f10ede950cba240b4b3faf8ac7
Ivan <vankata294n4@gmail.com>
2022-12-11 16:35:20 +0200

    setup db

--- project/backend/backend/settings.py
+++ project/backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
