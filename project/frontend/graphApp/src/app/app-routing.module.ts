a93dd7bcb0b02c793fe4c4f16bfdb00e0db8bbeb
Ivan <vankata294n4@gmail.com>
2022-12-12 11:07:53 +0200

    Added conctacts to router

--- project/frontend/graphApp/src/app/app-routing.module.ts
+++ project/frontend/graphApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './main/about/about.component';
+import { ContactComponent } from './main/contact/contact.component';
 import { IndexComponent } from './main/index/index.component';
 import { AddUserComponent } from './user/add-user/add-user.component';
 import { UserDetailsComponent } from './user/user-details/user-details.component';
@@ -22,6 +23,11 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+
+  {
+    path: 'contacts',
+    component: ContactComponent,
   },
 
   {
