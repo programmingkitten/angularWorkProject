57550579c5985b3df7014633b1ebc8c4ea93c1ca
Ivan <vankata294n4@gmail.com>
2022-12-12 11:48:03 +0200

    Added calculator component to router

--- project/frontend/graphApp/src/app/app-routing.module.ts
+++ project/frontend/graphApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './graph/calculator/calculator.component';
 import { AboutComponent } from './main/about/about.component';
 import { ContactComponent } from './main/contact/contact.component';
 import { IndexComponent } from './main/index/index.component';
@@ -23,6 +24,11 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+
+  {
+    path: 'calculator',
+    component: CalculatorComponent,
   },
 
   {
