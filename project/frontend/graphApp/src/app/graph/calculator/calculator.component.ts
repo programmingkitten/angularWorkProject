f988f1887f4bbac7621673abc214ec20665265ea
Ivan <vankata294n4@gmail.com>
2022-12-12 17:14:12 +0200

    Added variables for each chart

--- project/frontend/graphApp/src/app/graph/calculator/calculator.component.ts
+++ project/frontend/graphApp/src/app/graph/calculator/calculator.component.ts
@@ -15,7 +15,8 @@ export class CalculatorComponent{
   @ViewChild('barGraph') barGraph: ElementRef<HTMLCanvasElement> | undefined;
   @ViewChild('numberInput') numberInput: ElementRef | undefined;
   numbers: number[] = []; 
-
+  lineChart: Chart | undefined;
+  graphChart: Chart | undefined;
   constructor(private graphService: GraphService) {
     this.createGraphs()
   }
@@ -25,13 +26,13 @@ export class CalculatorComponent{
     this.numbers = this.graphService.colatzAlg(777)
     setTimeout(() => 
     {
-      this.graphService.createChar(this.lineGraph?.nativeElement, 'line',
+      this.lineChart = this.graphService.createChar(this.lineGraph?.nativeElement, 'line',
         this.numbers,
         this.graphService.rangeNum(1, this.numbers.length),
         'red', 2, 
         )
 
-      this.graphService.createChar(
+      this.graphChart = this.graphService.createChar(
         this.barGraph?.nativeElement, 'bar', this.graphService.groupNumbers(this.numbers),  
         ['0-100', '100-1k', '1k-10k', '10k-100k', '100k-1m'],'rgba(164, 7, 7, 0.858)', 1)
     }, 0)
