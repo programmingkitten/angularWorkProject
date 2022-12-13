28024b92e50605c749413440d5c6f6242f4d2298
Ivan <vankata294n4@gmail.com>
2022-12-13 19:31:28 +0200

    Api register now registers correctly

--- project/backend/user_auth/urls.py
+++ project/backend/user_auth/urls.py
@@ -2,10 +2,10 @@ from django.urls import re_path, include
 
 from django.contrib import admin
 from django.urls import path
-import views
+import user_auth.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', )
+    path('register', views.registerView.as_view())
 
 ]
--- project/backend/user_auth/views.py
+++ project/backend/user_auth/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from user_auth.serializers import UserSerializer
 
 
 class registerView(APIView):
-    pass
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
