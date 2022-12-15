5c4bd7c6204e2997f9c19bfcca82c505841dfa5d
Ivan <vankata294n4@gmail.com>
2022-12-15 22:26:12 +0200

    Added description field to user module

--- project/backend/user_auth/models.py
+++ project/backend/user_auth/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
     username = None
 
     USERNAME_FIELD = 'email'
