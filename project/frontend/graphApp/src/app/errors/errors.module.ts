dc5b2f5fa36453701c63f5ad6e02f7065d874a85
Ivan <vankata294n4@gmail.com>
2022-12-15 20:50:20 +0200

    Created erros module and no-access comp.

---
+++ project/frontend/graphApp/src/app/errors/errors.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NoAccessComponent } from './no-access/no-access.component';
+
+
+
+@NgModule({
+  declarations: [
+    NoAccessComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ErrorsModule { }
