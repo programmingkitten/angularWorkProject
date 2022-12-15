5b9555a810384d78e6e9ddd51e73d82f65eff90b
Ivan <vankata294n4@gmail.com>
2022-12-15 21:27:06 +0200

    styled not-found template

--- project/frontend/graphApp/src/app/errors/not-found/not-found.component.ts
+++ project/frontend/graphApp/src/app/errors/not-found/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss', '../no-access/no-access.component.scss']
 })
 export class NotFoundComponent {
 
