34f61a805fd43de8321ae6adedd02ec196b29709
Ivan <vankata294n4@gmail.com>
2022-12-15 22:27:09 +0200

    adding description field to serializer

--- project/backend/user_auth/serializers.py
+++ project/backend/user_auth/serializers.py
@@ -6,7 +6,7 @@ from user_auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'password']
+        fields = ['id', 'email', 'password', 'description']
         extra_kwargs = {
             'password': {'write_only': True}
         }
