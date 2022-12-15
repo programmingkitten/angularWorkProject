02051e52106112e138c7c49fd38eac90703daa59
Ivan <vankata294n4@gmail.com>
2022-12-15 14:37:49 +0200

    adding auth guard

---
+++ project/frontend/graphApp/src/app/auth/auth-guard.ts
@@ -0,0 +1,9 @@
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+export class AuthGuard implements CanActivate {
+constructor(private authService: AuthService, private router: Router) {}
+canActivate(route: ActivatedRouteSnapshot, state:RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  return true
+ }
+}
