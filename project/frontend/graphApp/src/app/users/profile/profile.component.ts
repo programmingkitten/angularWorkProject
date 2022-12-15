657bd9fc539df88f16a9c53fc8378c66009b53ee
Ivan <vankata294n4@gmail.com>
2022-12-15 22:05:53 +0200

    styled profile template

--- project/frontend/graphApp/src/app/users/profile/profile.component.ts
+++ project/frontend/graphApp/src/app/users/profile/profile.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { IUser } from '../user-interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit{
+  user: IUser = {
+    'email': '',
+  };
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit(): void {
+    this.setUser();
+  };
+
+  setUser() {
+    const data = this.authService.isLoggedIn().subscribe(
+      {
+        next: (data) => this.handleUserData(data),
+        error: (err) => {console.log(err)},
+      }
+    );
+  };
+
+  handleUserData(data: any) {
+    this.user.email = data.data.email.split('@')[0];
+  };
+};
