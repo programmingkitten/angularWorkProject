4cdc8d7ee1148027190df319fff0e1010be05ed8
Ivan <vankata294n4@gmail.com>
2022-12-15 14:44:17 +0200

    Creating auth interceptor

---
+++ project/frontend/graphApp/src/app/auth/auth-interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable, Provider } from "@angular/core";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { mergeMap, Observable, tap } from "rxjs";
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            tap((req => {
+                if (req instanceof HttpResponse) {
+                    console.log(req.body)
+                }
+            }))
+        )
+    }
+}
+
+export const appInterceptorProvider: Provider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AppInterceptor,
+    multi: true,
+}
