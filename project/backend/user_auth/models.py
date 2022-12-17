a45961494e30cc8d27830cfb1502e24ce36a44fe
Ivan <vankata294n4@gmail.com>
2022-12-17 16:34:43 +0200

    added creation date on backend feedback module

--- project/backend/user_auth/models.py
+++ project/backend/user_auth/models.py
@@ -15,7 +15,10 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+
 class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
     subject = models.CharField(max_length=40)
     message = models.TextField(max_length=1500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
